96cc2a7d17a6047999c736a0a9bca8b86c3489ba
Boddu Sesha Venkata Sai Ranga Akhilesh <bsvsrakhilesh01@gmail.com>
2026-04-20 12:49:44 +0530

    removed the review migration

--- backend/prisma/migrations/20260420110000_add_url_review_state/migration.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "Url"
-ADD COLUMN "lastReviewedAt" TIMESTAMP(3);
-
-ALTER TABLE "Url"
-ADD COLUMN "needsReview" BOOLEAN NOT NULL DEFAULT true;
-
-CREATE INDEX "Url_needsReview_idx" ON "Url"("needsReview");
-CREATE INDEX "Url_lastReviewedAt_idx" ON "Url"("lastReviewedAt");
